088cd1fa4b123ac1e4470d97e7855e5a741bfaf5
David Barcenas <daveepro@outlook.com>
2021-02-11 21:05:56 -0600

    Add routes and routerLink

--- countries/src/app/app-routing.module.ts
+++ countries/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CapitalComponent } from './countries/pages/capital/capital.component';
+import { CountryDetailComponent } from './countries/pages/country-detail/country-detail.component';
+import { CountryComponent } from './countries/pages/country/country.component';
+import { RegionComponent } from './countries/pages/region/region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: CountryComponent },
+  { path: 'region', component: RegionComponent },
+  { path: 'capital', component: CapitalComponent },
+  { path: 'country/:id', component: CountryDetailComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
