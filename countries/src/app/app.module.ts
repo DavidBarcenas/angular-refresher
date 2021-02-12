cebe8512bff3a2bd7494e930a4a03365da5f08be
David Barcenas <daveepro@outlook.com>
2021-02-11 19:48:49 -0600

    create modules and components

--- countries/src/app/app.module.ts
+++ countries/src/app/app.module.ts
@@ -3,16 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { CountriesModule } from './countries/countries.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    CountriesModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ countries/src/app/countries/countries.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CapitalComponent } from './pages/capital/capital.component';
+import { CountryComponent } from './pages/country/country.component';
+import { RegionComponent } from './pages/region/region.component';
+import { CountryDetailComponent } from './pages/country-detail/country-detail.component';
+
+@NgModule({
+  declarations: [
+    CapitalComponent,
+    CountryComponent,
+    RegionComponent,
+    CountryDetailComponent,
+  ],
+  imports: [CommonModule],
+  exports: [
+    CapitalComponent,
+    CountryComponent,
+    RegionComponent,
+    CountryDetailComponent,
+  ],
+})
+export class CountriesModule {}
