38657b642b635fd2be6e680b03a25ea229a4d891
David Barcenas <daveepro@outlook.com>
2021-02-17 08:31:06 -0600

    heroes: Create initial modules and components

---
+++ heroes/src/app/heroes/heroes.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AddHeroComponent } from './pages/add-hero/add-hero.component';
+import { HeroDetailComponent } from './pages/hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './pages/hero-search/hero-search.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeroListComponent } from './pages/hero-list/hero-list.component';
+
+@NgModule({
+  declarations: [
+    AddHeroComponent,
+    HeroDetailComponent,
+    HeroSearchComponent,
+    HomeComponent,
+    HeroListComponent,
+  ],
+  imports: [CommonModule],
+})
+export class HeroesModule {}
