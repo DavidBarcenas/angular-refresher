c0f364d1445e2f338f3f819bee899db74218208b
David Barcenas <daveepro@outlook.com>
2021-03-05 21:51:20 -0600

    heroes: Persist user session

--- heroes/src/app/app-routing.module.ts
+++ heroes/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ const routes: Routes = [
     path: 'heroes',
     loadChildren: () =>
       import('./heroes/heroes.module').then((m) => m.HeroesModule),
+    canActivate: [AuthGuard],
     canLoad: [AuthGuard],
   },
   { path: '404', component: ErrorPageComponent },
--- heroes/src/app/auth/services/auth.service.ts
+++ heroes/src/app/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { IAuth } from '../interfaces/auth.interface';
 
 @Injectable({
@@ -14,9 +14,24 @@ export class AuthService {
   }
   constructor(private http: HttpClient) {}
 
+  authVerify(): Observable<boolean> {
+    if (!localStorage.getItem('token')) {
+      return of(false);
+    }
+    return this.http.get<IAuth>('http://localhost:3000/usuarios/1').pipe(
+      map((auth) => {
+        this._auth = auth;
+        return true;
+      })
+    );
+  }
+
   login(): Observable<IAuth> {
-    return this.http
-      .get<IAuth>('http://localhost:3000/usuarios/1')
-      .pipe(tap((auth) => (this._auth = auth)));
+    return this.http.get<IAuth>('http://localhost:3000/usuarios/1').pipe(
+      tap((auth) => {
+        this._auth = auth;
+        localStorage.setItem('token', auth.id);
+      })
+    );
   }
 }
