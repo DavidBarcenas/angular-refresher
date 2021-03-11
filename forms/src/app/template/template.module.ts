a573d72f882b8d975d043b9abd4bac4ccd4d1cb0
David Barcenas <daveepro@outlook.com>
2021-03-10 21:31:17 -0600

    forms(template): Create basic form

--- forms/src/app/template/template.module.ts
+++ forms/src/app/template/template.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { TemplateRoutingModule } from './template-routing.module';
 import { SwitchesComponent } from './switches/switches.component';
@@ -11,7 +12,8 @@ import { BasicsComponent } from './basics/basics.component';
   declarations: [SwitchesComponent, DinamicsComponent, BasicsComponent],
   imports: [
     CommonModule,
-    TemplateRoutingModule
+    TemplateRoutingModule,
+    FormsModule
   ]
 })
 export class TemplateModule { }
