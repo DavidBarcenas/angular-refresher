6ce85f48fddc2534473c5aea334047ab3bc728c6
David Barcenas <daveepro@outlook.com>
2021-03-12 19:51:52 -0600

    forms(template): Create switches form

--- forms/src/app/template/switches/switches.component.ts
+++ forms/src/app/template/switches/switches.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styleUrls: ['./switches.component.scss'],
 })
-export class SwitchesComponent implements OnInit {
-  constructor() {}
+export class SwitchesComponent {
+  person = {
+    gender: 'F',
+    notifications: true,
+  };
 
-  ngOnInit(): void {}
+  terms = false;
 
   save() {}
 }
