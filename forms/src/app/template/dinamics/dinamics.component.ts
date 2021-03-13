7a3cd1e40fe7c04dc49dc51360cd5520046bdb6a
David Barcenas <daveepro@outlook.com>
2021-03-12 19:08:52 -0600

    forms(dynamics): Delete inputs

--- forms/src/app/template/dinamics/dinamics.component.ts
+++ forms/src/app/template/dinamics/dinamics.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Person {
+  name: string;
+  favorites: Favorite[];
+}
+
+interface Favorite {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
 })
-export class DinamicsComponent implements OnInit {
-  constructor() {}
+export class DinamicsComponent {
+  person: Person = {
+    name: 'Davee',
+    favorites: [
+      { id: 1, name: 'Metal Gear' },
+      { id: 2, name: 'Warzone' },
+    ],
+  };
 
-  ngOnInit(): void {}
+  save(form: NgForm) {
+    console.log('form', form.value);
+  }
 
-  save() {
-    console.log('form');
+  deleteFavorite(idx: number) {
+    this.person.favorites.splice(idx, 1);
   }
 }
