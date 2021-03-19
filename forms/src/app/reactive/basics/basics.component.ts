e74496901f16c37f8c1163018a9fb584d2ccc525
David Barcenas <daveepro@outlook.com>
2021-03-18 19:27:16 -0600

    forms(reactive): Ceate basic form

--- forms/src/app/reactive/basics/basics.component.ts
+++ forms/src/app/reactive/basics/basics.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
-  styleUrls: ['./basics.component.scss']
+  styleUrls: ['./basics.component.scss'],
 })
 export class BasicsComponent implements OnInit {
+  basicForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    price: new FormControl(''),
+    existing: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- forms/src/app/reactive/reactive.module.ts
+++ forms/src/app/reactive/reactive.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicsComponent } from './basics/basics.component';
 import { DinamicsComponent } from './dinamics/dinamics.component';
 import { SwitchesComponent } from './switches/switches.component';
 
-
 @NgModule({
   declarations: [BasicsComponent, DinamicsComponent, SwitchesComponent],
-  imports: [
-    CommonModule,
-    ReactiveRoutingModule
-  ]
+  imports: [CommonModule, ReactiveRoutingModule, ReactiveFormsModule],
 })
-export class ReactiveModule { }
+export class ReactiveModule {}
