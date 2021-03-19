3d6e556390b62d4d61e663a6fcf1db6078713d00
David Barcenas <daveepro@outlook.com>
2021-03-18 20:56:57 -0600

    forms(reactive): Start dynamic form

--- forms/src/app/reactive/dinamics/dinamics.component.ts
+++ forms/src/app/reactive/dinamics/dinamics.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamics',
   templateUrl: './dinamics.component.html',
-  styleUrls: ['./dinamics.component.scss']
+  styleUrls: ['./dinamics.component.scss'],
 })
 export class DinamicsComponent implements OnInit {
+  dynamicForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.dynamicForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      price: [
+        ,
+        [Validators.required, Validators.min(0), Validators.max(99.99)],
+      ],
+      existing: [, [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  getValidate(field: string) {
+    return (
+      this.dynamicForm.controls[field].errors &&
+      this.dynamicForm.controls[field].touched
+    );
+  }
+
+  save() {
+    if (this.dynamicForm.invalid) {
+      this.dynamicForm.markAllAsTouched();
+      return;
+    }
+
+    this.dynamicForm.reset();
+  }
 }
