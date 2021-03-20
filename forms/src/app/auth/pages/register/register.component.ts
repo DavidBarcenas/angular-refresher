bcef116f483891251373b43615181b6f2a376b30
David Barcenas <daveepro@outlook.com>
2021-03-19 20:45:24 -0600

    forms(validations): Validate name [pattern]

--- forms/src/app/auth/pages/register/register.component.ts
+++ forms/src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  namePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
 
-  constructor() { }
+  registerForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(this.namePattern)]],
+    email: [''],
+    username: [''],
+    password: [''],
+    passwordConfirm: [''],
+  });
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  validateField(field: string): boolean {
+    const form = this.registerForm.controls;
+    return form[field]?.touched && form[field]?.invalid;
   }
 
+  handleSubmit() {
+    this.registerForm.markAllAsTouched();
+  }
 }
