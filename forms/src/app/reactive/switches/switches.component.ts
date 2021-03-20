cc4a66f0ef99d08537a1cf26ec0dafae10327f1f
David Barcenas <daveepro@outlook.com>
2021-03-19 18:17:01 -0600

    forms(reactive): Start switches form [requiredTrue]

--- forms/src/app/reactive/switches/switches.component.ts
+++ forms/src/app/reactive/switches/switches.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
   templateUrl: './switches.component.html',
-  styleUrls: ['./switches.component.scss']
+  styleUrls: ['./switches.component.scss'],
 })
 export class SwitchesComponent implements OnInit {
+  switchesForm: FormGroup;
+  person = {
+    gender: 'F',
+    notification: true,
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) {
+    this.switchesForm = this.fb.group({
+      gender: ['M', Validators.required],
+      notifications: [true, Validators.requiredTrue],
+      terms: [false, Validators.requiredTrue],
+    });
   }
 
+  ngOnInit(): void {
+    this.switchesForm.patchValue(this.person);
+  }
+
+  save() {
+    console.log(this.switchesForm.value);
+  }
 }
